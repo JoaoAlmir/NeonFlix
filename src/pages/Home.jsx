e6bb05d8cf31ed3f14174bcf0552aa624ede8700
JoaoAlmir <joaoalmir@alu.ufc.br>
2024-12-26 06:53:18 -0300

    remove parallax

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import "./Home.css";
 import { FlipWordsDemo } from '../components/FlipWordsDemo';
 import { LampDemo } from '../components/LampDemo';
-import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 import { ThreeDCardDemo } from '../components/ThreeDCardDemo';
 import { BackgroundGradientAnimationDemo } from '../components/BackgroundGradientAnimationDemo';
 import imgPeople from '../assets/tvpeople.png';
@@ -12,37 +11,25 @@ import { HeroParallaxDemo } from '../components/HeroParallaxDemo';
 const Home = () => {
     return (
         <>
-            <Parallax pages={4}>
-                <ParallaxLayer offset={0} speed={0.5}>
-                    <div className='background'>
-                        <BackgroundGradientAnimationDemo />
-                    </div>
-                </ParallaxLayer>
+            <div className='background'>
+                <BackgroundGradientAnimationDemo />
+            </div>
 
-                <ParallaxLayer offset={0.99} speed={0.1}>
-                    <div className='background bg-gray-950'>
-                        <ThreeDCardDemo />
-                    </div>
-                </ParallaxLayer>
+            <div className='background bg-gray-950'>
+                <ThreeDCardDemo />
+            </div>
 
-                <ParallaxLayer offset={1.3} speed={0.6} >
-                    <div className='background-people'>
-                        <img src={imgPeople} alt="TV People" className="tv-people-image" />
-                    </div>
-                </ParallaxLayer>
+            <div className='background-people'>
+                <img src={imgPeople} alt="TV People" className="tv-people-image" />
+            </div>
 
-                <ParallaxLayer offset={1.8} speed={0.5}>
-                    <div className='background'>
-                        <LampDemo />
-                    </div>
-                </ParallaxLayer>
+            <div className='background'>
+                <LampDemo />
+            </div>
 
-                <ParallaxLayer offset={2.3} speed={0.5}>
-                    <div className='background'>
-                        <HeroParallaxDemo />
-                    </div>
-                </ParallaxLayer>
-            </Parallax>
+            <div className='background'>
+                <HeroParallaxDemo />
+            </div>
         </>
     );
 };
